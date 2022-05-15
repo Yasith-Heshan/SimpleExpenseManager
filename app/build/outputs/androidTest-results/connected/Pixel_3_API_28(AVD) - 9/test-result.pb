0a039f4ecc4c1d53a50b76e63c6bda685753a67e
Yasith-Heshan <81988554+Yasith-Heshan@users.noreply.github.com>
2022-05-15 13:05:46 +0530

    test cases

---
+++ app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/test-result.pb
@@ -0,0 +1,48 @@
+
+ñ
+k
+ApplicationTest&lk.ac.mrt.cse.dbs.simpleexpensemanagertestIncomeTransaction2ÙÒ‚”€Ê¾è:ÚÒ‚”Àüð5"ð
+
+logcatandroidÚ
+×/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/logcat-lk.ac.mrt.cse.dbs.simpleexpensemanager.ApplicationTest-testIncomeTransaction.txt"¬
+
+device-infoandroid‘
+Ž/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/device-info.pb"­
+
+device-info.meminfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/meminfo"­
+
+device-info.cpuinfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/cpuinfoò
+k
+ApplicationTest&lk.ac.mrt.cse.dbs.simpleexpensemanagertestExpenseTransaction2ÚÒ‚”€‘ÏE:ÚÒ‚”À•ŒF"ñ
+
+logcatandroidÛ
+Ø/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/logcat-lk.ac.mrt.cse.dbs.simpleexpensemanager.ApplicationTest-testExpenseTransaction.txt"¬
+
+device-infoandroid‘
+Ž/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/device-info.pb"­
+
+device-info.meminfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/meminfo"­
+
+device-info.cpuinfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/cpuinfoà
+b
+ApplicationTest&lk.ac.mrt.cse.dbs.simpleexpensemanagertestAddacount2ÚÒ‚”Àž†G:ÚÒ‚”Àœ³T"è
+
+logcatandroidÒ
+Ï/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/logcat-lk.ac.mrt.cse.dbs.simpleexpensemanager.ApplicationTest-testAddacount.txt"¬
+
+device-infoandroid‘
+Ž/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/device-info.pb"­
+
+device-info.meminfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/meminfo"­
+
+device-info.cpuinfoandroidŠ
+‡/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/cpuinfo*‘
+c
+test-results.logOcom.google.testing.platform.runtime.android.driver.AndroidInstrumentationDriver›
+˜/home/yasith/AndroidStudioProjects/SimpleExpenseManager/app/build/outputs/androidTest-results/connected/Pixel_3_API_28(AVD) - 9/testlog/test-results.log 2
+text/plain
--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -16,14 +16,75 @@
 
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest {
+
+    private static ExpenseManager expenseManager;
+
+    @Before
+    public void setUp(){
+        Context context = ApplicationProvider.getApplicationContext();
+        expenseManager = new PersistentExpenseManager(context);
+    }
+
+    @Test
+    public void testAddacount(){
+        expenseManager.addAccount("19022P","BOC","Yasith",1000.00);
+        List<String> accNumbers = expenseManager.getAccountNumbersList();
+        assertTrue(accNumbers.contains("19022P"));
+    }
+    @Test
+    public void testIncomeTransaction(){
+        expenseManager.addAccount("19022P","BOC","Yasith",1000.00);
+        try {
+            expenseManager.updateAccountBalance("190222P",12,03,2022, ExpenseType.INCOME,"100.00");
+
+            List<Transaction> transactions =  expenseManager.getTransactionLogs();
+            Transaction last = transactions.get(transactions.size()-1);
+            assertEquals("190222P",last.getAccountNo());
+            assertEquals(ExpenseType.INCOME,last.getExpenseType());
+            assertEquals(100.00,last.getAmount(),0.0);
+            assertEquals("12-04-2022",new SimpleDateFormat("dd-MM-yyyy").format(last.getDate()));
+        } catch (InvalidAccountException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testExpenseTransaction(){
+        expenseManager.addAccount("19022P","BOC","Yasith",1000.00);
+        try {
+            expenseManager.updateAccountBalance("190222P",12,03,2022, ExpenseType.EXPENSE,"100.00");
+
+            List<Transaction> transactions =  expenseManager.getTransactionLogs();
+            Transaction last = transactions.get(transactions.size()-1);
+            assertEquals("190222P",last.getAccountNo());
+            assertEquals(ExpenseType.EXPENSE,last.getExpenseType());
+            assertEquals(100.00,last.getAmount(),0.0);
+            assertEquals("12-04-2022",new SimpleDateFormat("dd-MM-yyyy").format(last.getDate()));
+        } catch (InvalidAccountException e) {
+            e.printStackTrace();
+        }
     }
 }
